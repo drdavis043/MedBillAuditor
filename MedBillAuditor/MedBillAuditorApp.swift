2eab76073897fec98356e8e5513e373e94d3f8e8
drdavis043 <drdavis043@gmail.com>
2026-02-08 14:32:42 -0500

    Initial Commit

---
+++ MedBillAuditor/MedBillAuditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedBillAuditorApp.swift
+//  MedBillAuditor
+//
+//  Created by Derek Davis on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct MedBillAuditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
